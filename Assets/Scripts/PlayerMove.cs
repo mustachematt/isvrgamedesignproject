506d2a3a0b2b5c4e4265331d4ee79c45bde1bf73
MustacheMatt <moustachematt18@gmail.com>
2020-09-20 15:24:06 -0400

    scene/player has working pickup and alien hands

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class PlayerMove : MonoBehaviour
+{
+    SteamVR_TrackedObject trackedObject;
+    
+
+    private void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
