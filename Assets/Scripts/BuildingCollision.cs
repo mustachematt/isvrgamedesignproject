42da993a18d683228cfd3465de2fb937b8caeeb6
nfahey1 <nfahey1@kent.edu>
2020-09-21 11:05:22 -0400

    Building Collision
    Added collision mechanics of buildings

---
+++ Assets/Scripts/BuildingCollision.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingCollision : MonoBehaviour
+{
+    public GameObject rubbleEffect;
+    public int sinkDistance;
+
+    
+
+    void OnTriggerEnter(Collider hand)
+    {
+        if(hand.tag == "hand")
+        {
+            if(gameObject.tag == "skyscraper")
+            {
+                SkyscraperHit(hand.transform.position);
+            }
+            else if (gameObject.tag == "house")
+            {
+                HouseDestroy();
+            }
+        }
+    }
+
+    //function called when skyscraper is hit with hand
+    void SkyscraperHit(Vector3 handPosition)
+    {
+        Instantiate(rubbleEffect, handPosition, Quaternion.identity);
+
+
+        /////
+        //make building go down
+        /////
+
+        if(transform.position.y == 0)
+            SkyscraperDestroy();
+    }
+
+    //function called when skyscraper is destroyed
+    void SkyscraperDestroy()
+    {
+        Destroy(gameObject);
+    }
+
+    //function for when house is hit with hand
+    void HouseDestroy()
+    {
+        Instantiate(rubbleEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+
+}
---
+++ Assets/Scripts/EffectDespawn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectDespawn : MonoBehaviour
+{
+
+    int timeleft;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeleft = 30;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeleft--;
+        if (timeleft <= 0)
+            Destroy(gameObject);
+    }
+}
