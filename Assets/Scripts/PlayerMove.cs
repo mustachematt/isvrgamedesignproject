14ca7c2ac00d39c1f4fa75cfeb5564f8d564eb8b
MustacheMatt <moustachematt18@gmail.com>
2020-09-27 14:48:26 -0400

    changed movement and building smash
    the player now travels using the trackpad on the right controller relative to the forward vector of the player's camera. Snap rotation has also been added back on the left controller. The monster can now also smash buildings.

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -5,20 +5,30 @@ using Valve.VR;
 
 public class PlayerMove : MonoBehaviour
 {
+    public GameObject vrCamera;
     public SteamVR_Action_Vector2 walk;
-    public SteamVR_Input_Sources handType;
-    public float walkSpeed = 2f;
+    public SteamVR_Input_Sources rightHand;
+    public float walkSpeed = 20f;
+
 
     private void Start()
     {
-        walk.AddOnAxisListener(WalkDirection, handType);
+        walk.AddOnAxisListener(GetWalkDirection, rightHand);
     }
 
-    public void WalkDirection(SteamVR_Action_Vector2 fromAction, SteamVR_Input_Sources fromSource, Vector2 axis, Vector2 delta)
+
+    public void GetWalkDirection(SteamVR_Action_Vector2 fromAction, SteamVR_Input_Sources fromSource, Vector2 axis, Vector2 delta)
     {
-        Debug.Log(walk.axis);
-        transform.position = new Vector3(transform.position.x + (walk.axis.x * walkSpeed * Time.deltaTime),
-            transform.position.y,
-            transform.position.z + (walk.axis.y * walkSpeed * Time.deltaTime));
+        // convert the vector from the thumb position on the controller to 3d
+        Vector3 thumbVector = new Vector3(walk.axis.x, 0, walk.axis.y);
+
+        // get the angle of the camera relative to the forward position
+        float cameraAngle = Vector3.SignedAngle(Vector3.forward, vrCamera.transform.forward, Vector3.up);
+
+        // rotate the thumb vector that many degrees to get the walk vector
+        Vector3 walkVector = Quaternion.AngleAxis(cameraAngle, transform.up) * thumbVector;
+        
+        // move that direction
+        GetComponent<Rigidbody>().AddForce(walkVector * walkSpeed);
     }
 }
--- Assets/SteamVR_Input/SteamVR_Input_Actions.cs
+++ Assets/SteamVR_Input/SteamVR_Input_Actions.cs
@@ -55,6 +55,8 @@ namespace Valve.VR
         
         private static SteamVR_Action_Vector2 p_monster_MovementAxis;
         
+        private static SteamVR_Action_Vector2 p_monster_TurnAxis;
+        
         public static SteamVR_Action_Boolean default_InteractUI
         {
             get
@@ -207,6 +209,14 @@ namespace Valve.VR
             }
         }
         
+        public static SteamVR_Action_Vector2 monster_TurnAxis
+        {
+            get
+            {
+                return SteamVR_Actions.p_monster_TurnAxis.GetCopy<SteamVR_Action_Vector2>();
+            }
+        }
+        
         private static void InitializeActionArrays()
         {
             Valve.VR.SteamVR_Input.actions = new Valve.VR.SteamVR_Action[] {
@@ -228,7 +238,8 @@ namespace Valve.VR
                     SteamVR_Actions.buggy_Brake,
                     SteamVR_Actions.buggy_Reset,
                     SteamVR_Actions.mixedreality_ExternalCamera,
-                    SteamVR_Actions.monster_MovementAxis};
+                    SteamVR_Actions.monster_MovementAxis,
+                    SteamVR_Actions.monster_TurnAxis};
             Valve.VR.SteamVR_Input.actionsIn = new Valve.VR.ISteamVR_Action_In[] {
                     SteamVR_Actions.default_InteractUI,
                     SteamVR_Actions.default_GrabPinch,
@@ -247,7 +258,8 @@ namespace Valve.VR
                     SteamVR_Actions.buggy_Brake,
                     SteamVR_Actions.buggy_Reset,
                     SteamVR_Actions.mixedreality_ExternalCamera,
-                    SteamVR_Actions.monster_MovementAxis};
+                    SteamVR_Actions.monster_MovementAxis,
+                    SteamVR_Actions.monster_TurnAxis};
             Valve.VR.SteamVR_Input.actionsOut = new Valve.VR.ISteamVR_Action_Out[] {
                     SteamVR_Actions.default_Haptic};
             Valve.VR.SteamVR_Input.actionsVibration = new Valve.VR.SteamVR_Action_Vibration[] {
@@ -271,7 +283,8 @@ namespace Valve.VR
             Valve.VR.SteamVR_Input.actionsVector2 = new Valve.VR.SteamVR_Action_Vector2[] {
                     SteamVR_Actions.platformer_Move,
                     SteamVR_Actions.buggy_Steering,
-                    SteamVR_Actions.monster_MovementAxis};
+                    SteamVR_Actions.monster_MovementAxis,
+                    SteamVR_Actions.monster_TurnAxis};
             Valve.VR.SteamVR_Input.actionsVector3 = new Valve.VR.SteamVR_Action_Vector3[0];
             Valve.VR.SteamVR_Input.actionsSkeleton = new Valve.VR.SteamVR_Action_Skeleton[] {
                     SteamVR_Actions.default_SkeletonLeftHand,
@@ -290,7 +303,8 @@ namespace Valve.VR
                     SteamVR_Actions.buggy_Throttle,
                     SteamVR_Actions.buggy_Brake,
                     SteamVR_Actions.buggy_Reset,
-                    SteamVR_Actions.monster_MovementAxis};
+                    SteamVR_Actions.monster_MovementAxis,
+                    SteamVR_Actions.monster_TurnAxis};
         }
         
         private static void PreInitActions()
@@ -314,6 +328,7 @@ namespace Valve.VR
             SteamVR_Actions.p_buggy_Reset = ((SteamVR_Action_Boolean)(SteamVR_Action.Create<SteamVR_Action_Boolean>("/actions/buggy/in/Reset")));
             SteamVR_Actions.p_mixedreality_ExternalCamera = ((SteamVR_Action_Pose)(SteamVR_Action.Create<SteamVR_Action_Pose>("/actions/mixedreality/in/ExternalCamera")));
             SteamVR_Actions.p_monster_MovementAxis = ((SteamVR_Action_Vector2)(SteamVR_Action.Create<SteamVR_Action_Vector2>("/actions/monster/in/MovementAxis")));
+            SteamVR_Actions.p_monster_TurnAxis = ((SteamVR_Action_Vector2)(SteamVR_Action.Create<SteamVR_Action_Vector2>("/actions/monster/in/TurnAxis")));
         }
     }
 }
