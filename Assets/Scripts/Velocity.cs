ff5b7fbb82701022d1308badc5d14bb4b0e7fa94
MeldinBekticKSU <63599488+MeldinBekticKSU@users.noreply.github.com>
2020-09-29 14:16:59 -0400

    Added Velocity plus more.
    Made it so you have to go at a certain speed to destroy the buildings now. Also I shrunk the rubble effect a bit so that it doesn't blind the player, added a new background sky. I think a night theme would be nice for our game since we can lights and stuff down the road. We can add in a moon later.

---
+++ Assets/Scripts/Velocity.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Velocity : MonoBehaviour
+{
+    public float speed;
+    public float delayUpdate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(velocity());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator velocity()
+    {
+        YieldInstruction waitTime = new WaitForSeconds(delayUpdate);
+        Vector3 lastPos = transform.position;
+        float lastTime = Time.time;
+
+        while (enabled)
+        {
+            yield return waitTime;
+
+            var deltaPos = (transform.position - lastPos).magnitude;
+            var deltaTime = Time.time - lastTime;
+
+            if (Mathf.Approximately(deltaPos, 0f))
+                deltaPos = 0f;
+
+            speed = deltaPos / deltaTime;
+
+            lastPos = transform.position;
+            lastTime = Time.time;
+        }
+    }
+}
