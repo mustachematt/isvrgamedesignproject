68f91f40ffdd60b6fda1b4d6c050045d216ff7f5
nfahey1 <nfahey1@kent.edu>
2020-10-26 08:07:55 -0400

    Tank spawning and movement
    Added more buildings to map layout. Finished complete navmesh for tanks to travel on roads. Created objects to spawn tanks from military bases and tunnels. Created tank movement AI with a patrol phase, a stationary phase, and a chasing phase.

---
+++ Assets/Scripts/tankSpawn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tankSpawn : MonoBehaviour
+{
+    public GameObject tank;
+    int spawnChance = ( 20  *60); //determines the chance each frame that a tank will be spawned 
+
+    
+    void Update()
+    {
+        if(Random.Range(0, spawnChance) == 1)
+        {
+            Instantiate(tank, transform.position, Quaternion.identity);
+        }
+    }
+}
