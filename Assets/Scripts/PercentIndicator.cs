7a7ca53e155b6ca4d2e2915e1499c371b245340f
nfahey1 <nfahey1@kent.edu>
2020-11-13 11:10:35 -0500

    updated ui
    Updated ui text layout and made it so health and destruction percent are scroll bars. Also added a marker on percent bar to show the needed destruction percent that will move when the requirement is changed. Added playerprefs high score that is displayed on ui. Also reworked navmesh so all major roads are traversable by tanks. Also moved cars so they no longer interfere with tank navmesh.

---
+++ Assets/Scripts/PercentIndicator.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PercentIndicator : MonoBehaviour
+{
+    public GameObject counter;
+    float needed;
+
+    void Start()
+    {
+        needed = counter.GetComponent<GlobalCounterScript>().winPercent;
+        if(needed == 0.05)
+        {
+            gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(-95,201,0);
+        }
+        else if(needed == 0.1)
+        {
+            gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(-85, 201, 0);
+        }
+        else if (needed == 0.15)
+        {
+            gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(-75, 201, 0);
+        }
+        else if (needed == 0.2)
+        {
+            gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(-64, 201, 0);
+        }
+        else if (needed == 0.25)
+        {
+            gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(-54, 201, 0);
+        }
+        else if (needed == 0.3)
+        {
+            gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(-44, 201, 0);
+        }
+        else if (needed == 0.35)
+        {
+            gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(-33, 201, 0);
+        }
+        else if (needed == 0.4)
+        {
+            gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(-23, 201, 0);
+        }
+        else if (needed == 0.45)
+        {
+            gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(-12, 201, 0);
+        }
+        else if (needed == 0.5)
+        {
+            gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(-2, 201, 0);
+        }
+        else if (needed == 0.55)
+        {
+            gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(8, 201, 0);
+        }
+        else if (needed == 0.6)
+        {
+            gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(18, 201, 0);
+        }
+        else if (needed == 0.65)
+        {
+            gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(29, 201, 0);
+        }
+        else if (needed == 0.7)
+        {
+            gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(39, 201, 0);
+        }
+        else if (needed == 0.75)
+        {
+            gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(49, 201, 0);
+        }
+        else if (needed == 0.8)
+        {
+            gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(60, 201, 0);
+        }
+        else if (needed == 0.85)
+        {
+            gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(70, 201, 0);
+        }
+        else if (needed == 0.9)
+        {
+            gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(80, 201, 0);
+        }
+        else if (needed == 0.95)
+        {
+            gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(91, 201, 0);
+        }
+        else
+        {
+            gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(101, 201, 0);
+        }
+
+    }
+    void Update()
+    {
+        bool won = counter.GetComponent<GlobalCounterScript>().gotNeededPercent;
+        if (won == false)
+        {
+            //color blue
+            gameObject.GetComponent<Image>().color = new Color32(54, 56, 204, 255);
+        }
+        else
+        {
+            //color purple
+            gameObject.GetComponent<Image>().color = new Color32(248, 12, 238, 255);
+        }
+    }
+}
