6f8aeab8c5c618edec0b0b4388d96ea9fadc1469
nfahey1 <nfahey1@kent.edu>
2020-12-08 16:35:28 -0500

    Text Changes And Reset Button
    did some minor text fixes, added unlock text that tells how to unlock difficulty, added score and score reset to main menu

---
+++ Assets/Scripts/UI/ResetScore.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResetScore : MonoBehaviour
+{
+    public Text score;
+
+    void Start()
+    {
+        score.text = PlayerPrefs.GetInt("highScore", 0).ToString();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "hand")
+        {
+            PlayerPrefs.SetInt("highScore", 0);
+            score.text = PlayerPrefs.GetInt("highScore", 0).ToString();
+        }
+
+
+    }
+}
