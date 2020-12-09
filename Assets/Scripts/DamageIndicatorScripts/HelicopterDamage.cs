2160d5e1fcde27382110a5d73838617902b892dd
MustacheMatt <moustachematt18@gmail.com>
2020-12-08 21:18:50 -0500

    haptics, hot dog truck bug fix
    - added haptics for when the player gets hit by projectiles. demonstrates where on the player's body they got hit relative to where the camera is facing
    - fixed a bug where the hot dog truck had a null reference exception when colliding with tagged objects. Did this by re-enabling the VehicleThrow script on the hotdog truck prefab as it only re-enables the Start function which resolves the reference.
    - created a system where the menu raises up and down in increments depending on the height of the camera relative to the menu; if it gets too far away, it will be raised or lowered appropriately.
    - Minor scene changes

--- Assets/Scripts/DamageIndicatorScripts/HelicopterDamage.cs
+++ Assets/Scripts/DamageIndicatorScripts/HelicopterDamage.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
 
 public class HelicopterDamage : MonoBehaviour
 {
     public int damage;
-
     public HelicopterMovement heliParent = null;
+    public SteamVR_Action_Vibration hapticAction;
 
     void Start()
     {
@@ -20,6 +21,43 @@ public class HelicopterDamage : MonoBehaviour
         {
             GameObject.Find("GlobalCounter").GetComponent<GlobalCounterScript>().TakeDamage(damage);
             heliParent.damageIndicatorOn = true;
+
+            /*
+             * Haptics...
+             */
+
+            Vector3 bodyHitDirection = transform.position - other.gameObject.transform.position;
+            bodyHitDirection.Normalize();
+
+            Transform camera = GameObject.FindGameObjectWithTag("MainCamera").transform;
+
+            float distance = bodyHitDirection.x - camera.forward.normalized.x;
+            if (distance < -1)
+            {
+                float excess = Mathf.Abs(distance + 1);
+                distance += excess;
+            }
+            if (distance > 1)
+            {
+                float excess = distance - 1;
+                distance -= excess;
+            }
+            distance /= 2;
+
+            float rhsAmp = 0.5f, lhsAmp = 0.5f;
+            if (bodyHitDirection.z >= 0)
+            {
+                rhsAmp += distance;
+                lhsAmp -= distance;
+            }
+            if (bodyHitDirection.z < 0)
+            {
+                rhsAmp -= distance;
+                lhsAmp += distance;
+            }
+
+            hapticAction.Execute(0, 0.5f, 150, rhsAmp, SteamVR_Input_Sources.RightHand);
+            hapticAction.Execute(0, 0.5f, 150, lhsAmp, SteamVR_Input_Sources.LeftHand);
         }
     }
 }
